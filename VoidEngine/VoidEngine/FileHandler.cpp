62825dc283c1424e1e6de98e79c21f94b3c34c63
Ruben Telles <rctelles@ucsc.edu>
2014-04-21 21:24:39 -0700

    filehandling
    added more to the functionality of return the level
    but there are errors

--- VoidEngine/VoidEngine/FileHandler.cpp
+++ VoidEngine/VoidEngine/FileHandler.cpp
@@ -1,15 +1,20 @@
 #include "FileHandler.h"
 
-void FileHandler() {
+Level FileHandler::ReadFile(string filename)
+{
 	string line;
 	stringstream ss;
 	string object, tile, edges, points, neibors, nums;
 	int temp;
+	float temp2;
 	int tileID;
 	float vert[3];
 	list<float> vertices;
 	list<int> neighbors;
-	ifstream myfile("hole.00.db");
+	list<Tile> tiles;
+	Level::LevelObject tee;
+	Level::LevelObject cup;
+	ifstream myfile(filename);
 	if (myfile.is_open())
 	{
 		while (getline(myfile, line))
@@ -18,59 +23,93 @@ void FileHandler() {
 			ss.clear();
 			ss.str(line);
 
+
 			// separation of data into separate strings
 			ss >> object;
-			ss >> tile;
-			istringstream(tile) >> tileID;
-			ss >> edges;
-			// caculates how many points current tile has for reading correct number of points from data file
-			int numEdges;
-			istringstream(edges) >> numEdges;
-			//int numPoints = numEdges * 3;
-			//adds point to list of floats
-			for (int i = 0; i < numEdges; i++)
+			if (object == "tee")
 			{
-				
-				//vertices.push_back(temp);
+				ss >> tile;
+				istringstream(tile) >> tileID;
 				for (int i = 0; i < 3; i++)
 				{
 					ss >> nums;
 					points.append(nums);
-					istringstream(nums) >> temp;
+					istringstream(nums) >> temp2;
+					vert[i] = temp2;
 					
-					vert[i] = temp;
-
 				}
-					vertices.push_back(vert[3]);
+				
+				tee.position[0] = vert[0];
+				tee.position[1] = vert[1];
+				tee.position[2] = vert[2];
+				tee.tileID = tileID;
 
+				
 			}
-			// same as above but this time for edges
-			for (int i = 0; i < numEdges; i++)
+			else if (object == "cup")
 			{
-				ss >> nums;
-				neibors.append(nums);
-				istringstream(nums) >> temp;
-				neighbors.push_back(temp);
-
+				ss >> tile;
+				istringstream(tile) >> tileID;
+				for (int i = 0; i < 3; i++)
+				{
+					ss >> nums;
+					points.append(nums);
+					istringstream(nums) >> temp2;
+					vert[i] = temp2;
+				}
+				
+				cup.position[0] = vert[0];
+				cup.position[1] = vert[1];
+				cup.position[2] = vert[2];
+				cup.tileID = tileID;
 			}
-			// output of strings for testing
-			cout << "object: " << object << endl;
-			cout << "tileID:" << tile << endl;
-			cout << "edges:" << edges << endl;
-			cout << "points:" << points << endl;
-			cout << "neighbors:" << neibors << endl;
-			points.clear();
-			neibors.clear();
+			else
+			{
+				ss >> tile;
+				istringstream(tile) >> tileID;
+				ss >> edges;
+				// caculates how many points current tile has for reading correct number of points from data file
+				int numEdges;
+				istringstream(edges) >> numEdges;
+				for (int i = 0; i < numEdges; i++)
+				{
+					for (int i = 0; i < 3; i++)
+					{
+						ss >> nums;
+						points.append(nums);
+						istringstream(nums) >> temp2;
+						vert[i] = temp2;
+					}
+					vertices.push_back(vert);
+				}
+				// same as above but this time for edges
+				for (int i = 0; i < numEdges; i++)
+				{
+					ss >> nums;
+					neibors.append(nums);
+					istringstream(nums) >> temp;
+					neighbors.push_back(temp);
+				}
+				// output of strings for testing
+				//cout << "object: " << object << endl;
+				//cout << "tileID:" << tile << endl;
+				//cout << "edges:" << edges << endl;
+				//cout << "points:" << points << endl;
+				//cout << "neighbors:" << neibors << endl;
+				//points.clear();
+				//neibors.clear();
 
+				Tile *newtile = new Tile(tileID, vertices, neighbors);
+				tiles.push_back(*newtile);
+			}
+			return Level(tiles,tee,cup);
 
-
+				
 		}
-
 		myfile.close();
 	}
-
 	else cout << "Unable to open file";
-
-	//return 0;
 }
+
+
 
