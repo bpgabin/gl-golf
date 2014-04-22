fbceaf5a521ca6641b33608a40153649548756a0
Brian Gabin <bpgabin@gmail.com>
2014-04-21 17:36:20 -0700

    Implemented basic Tile and Level class

--- VoidEngine/VoidEngine/Tile.cpp
+++ VoidEngine/VoidEngine/Tile.cpp
@@ -1,1 +1,23 @@
 #include "Tile.h"
+
+Tile::Tile(int ID, list<float[3]> vertices, list<int> neighbors)
+{
+    mID = ID;
+    mVertices = vertices;
+    mNeighbors = neighbors;
+}
+
+list<float[3]> Tile::getVertices() const
+{
+    return mVertices;
+}
+
+list<int> Tile::getNeighbors() const
+{
+    return mNeighbors;
+}
+
+int Tile::getID() const
+{
+    return mID;
+}
